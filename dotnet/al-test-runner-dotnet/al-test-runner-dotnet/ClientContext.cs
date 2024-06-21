9b941be01efa4a96fd2423050595adb4d0b2a5e0
Jakub Vaňák <jakubv@navipartner.dk>
2024-06-21 22:50:36 +0400

    .net wrapper of MS ClientSession library - wip

---
+++ dotnet/al-test-runner-dotnet/al-test-runner-dotnet/ClientContext.cs
@@ -0,0 +1,280 @@
+﻿using System.Net;
+using System.Reflection;
+using Microsoft.Dynamics.Framework.UI.Client;
+using Microsoft.Dynamics.Framework.UI.Client.Interactions;
+
+namespace NaviPartner.ALTestRunner
+{
+    public class ClientContext
+    {
+        protected ClientSession ClientSession { get; private set; }
+        protected string Culture { get; private set; }
+        protected ClientLogicalForm OpenedForm { get; private set; }
+        protected string OpenedFormName { get; private set; }
+        private ClientLogicalForm PsTestRunnerCaughtForm;
+        
+
+        public ClientContext(string serviceUrl, AuthenticationScheme authenticationScheme, ICredentials credential,
+            TimeSpan interactionTimeout, string culture) : base()
+        {
+            Initialize(serviceUrl, authenticationScheme, credential, interactionTimeout, culture);
+        }
+
+        public void Initialize(string serviceUrl, AuthenticationScheme authenticationScheme, ICredentials credential,
+            TimeSpan interactionTimeout, string culture)
+        {
+            ServicePointManager.SetTcpKeepAlive(true, (int)TimeSpan.FromMinutes(30).TotalMilliseconds, (int)TimeSpan.FromMinutes(30).TotalMilliseconds);
+            SslVerification.Disable();
+
+            var clientServicesUrl = serviceUrl;
+
+            if ((!clientServicesUrl.Contains("/cs/")) && (!clientServicesUrl.Contains("/cs?")))
+            {
+                if (clientServicesUrl.Contains("?"))
+                {
+                    clientServicesUrl = clientServicesUrl.Insert(clientServicesUrl.LastIndexOf('?'), "cs/");
+                }
+                else
+                {
+                    clientServicesUrl = clientServicesUrl.TrimEnd('/');
+                    clientServicesUrl = clientServicesUrl + "/cs/";
+                }
+            }
+
+            var addressUri = new Uri(clientServicesUrl);
+            var jsonClient = new JsonHttpClient(addressUri, credential, authenticationScheme);
+
+            var httpClientField = typeof(JsonHttpClient).GetField("httpClient", BindingFlags.NonPublic | BindingFlags.Instance);
+            var httpClient = (HttpClient)httpClientField.GetValue(jsonClient);
+            httpClient.Timeout = interactionTimeout;
+
+
+            this.ClientSession = new ClientSession(jsonClient, new NonDispatcher(), new TimerFactory<TaskTimer>());
+            this.Culture = culture;
+            this.OpenSession();
+        }
+
+        protected void OpenSession()
+        {
+            var csParams = new ClientSessionParameters();
+            csParams.CultureId = Culture;
+            csParams.UICultureId = Culture;
+            csParams.AdditionalSettings.Add("IncludeControlIdentifier", true);
+
+            this.ClientSession.MessageToShow += Cs_MessageToShow;
+            this.ClientSession.CommunicationError += Cs_CommunicationError;
+            this.ClientSession.UnhandledException += Cs_UnhandledException;
+            this.ClientSession.InvalidCredentialsError += Cs_InvalidCredentialsError;
+            this.ClientSession.UriToShow += Cs_UriToShow;
+            this.ClientSession.DialogToShow += Cs_DialogToShow;
+
+            this.ClientSession.SessionReady += ClientSession_SessionReady;
+            this.ClientSession.OpenSessionAsync(csParams);
+            this.AwaitState(ClientSessionState.Ready);
+        }
+
+        public virtual void CloseSession()
+        {
+            if (ClientSession != null)
+            {
+                if (ClientSession.State.HasFlag(ClientSessionState.Ready | ClientSessionState.Busy | ClientSessionState.InError | ClientSessionState.TimedOut))
+                {
+                    CloseAllForms();
+                    OpenedForm = null;
+                    OpenedFormName = "";
+                    ClientSession.CloseSessionAsync();
+                }
+            }
+        }
+
+        private void ClientSession_SessionReady(object? sender, SessionInfoEventArgs e)
+        {
+            Console.WriteLine("Client session ready ...");
+        }
+
+        protected void AwaitState(ClientSessionState state)
+        {
+            while (this.ClientSession.State != state)
+            {
+                Thread.Sleep(100);
+
+                switch (this.ClientSession.State)
+                {
+                    case ClientSessionState.InError:
+                        throw new Exception("ClientSession in Error state");
+                    case ClientSessionState.TimedOut:
+                        throw new Exception("ClientSession time out");
+                    case ClientSessionState.Uninitialized:
+                        throw new Exception("ClientSession is Unitialized");
+                }
+
+                if (this.ClientSession.LastException != null)
+                {
+                    Console.WriteLine(this.ClientSession.LastException.Message);
+                }
+            }
+        }
+
+        public ClientLogicalForm OpenForm(int page)
+        {
+            if ((OpenedForm == null) || (String.IsNullOrEmpty(OpenedForm.Name)) || (OpenedForm.Name != OpenedFormName))
+            {
+                if ((OpenedForm != null) && (OpenedForm.Name != OpenedFormName))
+                {
+                    CloseForm(OpenedForm);
+                }
+
+                var interaction = new OpenFormInteraction();
+                interaction.Page = page.ToString();
+                OpenedForm = InvokeInteractionAndCatchForm(interaction);
+                OpenedFormName = OpenedForm.Name;
+            }
+            return OpenedForm;
+        }
+
+        public void CloseForm(ClientLogicalForm form)
+        {
+            this.InvokeInteraction(new CloseFormInteraction(form));
+        }
+
+        public ClientLogicalForm[] GetAllForms()
+        {
+            return this.ClientSession.OpenedForms.ToArray<ClientLogicalForm>();
+        }
+
+        public void InvokeInteraction(ClientInteraction interaction)
+        {
+            this.ClientSession.InvokeInteractionAsync(interaction);
+            this.AwaitState(ClientSessionState.Ready);
+        }
+
+        public ClientLogicalForm InvokeInteractionAndCatchForm(ClientInteraction interaction)
+        {
+            PsTestRunnerCaughtForm = null;
+            this.ClientSession.FormToShow += ClientSession_FormToShow;
+
+            try
+            {
+                this.InvokeInteraction(interaction);                
+
+                if (PsTestRunnerCaughtForm == null) {
+                    this.CloseAllWarningForms();
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = ex.Message;
+                var failedItem = ex.Source;
+                Console.WriteLine($"Error: {errorMessage} Item: {failedItem}");
+            }
+            finally
+            {
+                this.ClientSession.FormToShow -= ClientSession_FormToShow;
+            }
+
+            var form = PsTestRunnerCaughtForm;
+            PsTestRunnerCaughtForm = null;
+            return form;
+        }
+
+        public void CloseAllForms()
+        {
+            foreach (var form in this.GetAllForms())
+            {
+                this.CloseForm(form);
+            }
+        }
+
+        public void CloseAllErrorForms()
+        {
+            foreach (var form in this.GetAllForms())
+            {
+                if (form.ControlIdentifier == "00000000-0000-0000-0800-0000836bd2d2")
+                {
+                    this.CloseForm(form);
+                }
+            }
+        }
+        public void CloseAllWarningForms()
+        {
+            foreach (var form in this.GetAllForms())
+            {
+                if (form.ControlIdentifier == "00000000-0000-0000-0300-0000836bd2d2") {
+                    this.CloseForm(form);
+                }
+            }
+        }
+
+        protected ClientLogicalControl GetControlByName(ClientLogicalControl control, string name)
+        {
+            return control.ContainedControls.Where(clc => (clc.Name == name)).First();
+        }
+
+        protected ClientActionControl GetActionByName(ClientLogicalControl control, string name)
+        {
+            return (ClientActionControl)control.ContainedControls.Where(c => (c.GetType() == typeof(ClientActionControl)) && c.Name == name).First();
+        }
+
+        protected void InvokeAction(ClientActionControl action)
+        {
+            InvokeInteraction(new InvokeActionInteraction(action));
+        }
+
+        protected void SaveValue(ClientLogicalControl control, string newValue)
+        {
+            this.InvokeInteraction(new SaveValueInteraction(control, newValue));
+        }
+
+        private void ClientSession_FormToShow(object? sender, ClientFormToShowEventArgs e)
+        {
+            PsTestRunnerCaughtForm = e.FormToShow;
+        }
+
+        private void Cs_DialogToShow(object? sender, ClientDialogToShowEventArgs e)
+        {
+            var form = e.DialogToShow;
+            if (form.ControlIdentifier == "00000000-0000-0000-0800-0000836bd2d2") {
+                var errorControl = form.ContainedControls.Where(c => c.GetType() == typeof(ClientStaticStringControl)).First();
+                HandleClientSessionError($"ERROR: {errorControl.StringValue}", true);
+            }
+            if (form.ControlIdentifier == "00000000-0000-0000-0300-0000836bd2d2") {
+                var errorControl = form.ContainedControls.Where(c => c.GetType() == typeof(ClientStaticStringControl)).First();
+                Console.WriteLine($"WARNING: {errorControl.StringValue}");
+            }
+        }
+
+        private void Cs_UriToShow(object? sender, ClientUriToShowEventArgs e)
+        {
+            Console.WriteLine($"UriToShow : {e.UriToShow}");
+        }
+
+        private void Cs_InvalidCredentialsError(object? sender, MessageToShowEventArgs e)
+        {
+            HandleClientSessionError("InvalidCredentialsError", true);
+        }
+
+        private void Cs_UnhandledException(object? sender, ExceptionEventArgs e)
+        {
+            HandleClientSessionError($"UnhandledException: {e.Exception}", true);
+        }
+
+        private void Cs_CommunicationError(object? sender, ExceptionEventArgs e)
+        {
+            HandleClientSessionError($"CommunicationError: {e.Exception}", true);
+        }
+
+        private void Cs_MessageToShow(object? sender, MessageToShowEventArgs e)
+        {
+            Console.WriteLine($"Message: {e.Message}");
+        }
+
+        private void HandleClientSessionError(string errorMsg, bool? throwError)
+        {
+            Console.WriteLine($"ERROR: {errorMsg}");
+            if (throwError == true)
+            {
+                throw new Exception(errorMsg);
+            }
+        }
+    }
+}
---
+++ dotnet/al-test-runner-dotnet/al-test-runner-dotnet/TestRunner.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using BCClientLib.libs;
+using Microsoft.Dynamics.Framework.UI.Client;
+
+namespace NaviPartner.ALTestRunner
+{
+    public class TestRunner : ClientContext
+    {
+        public const string AllTestsExecutedString = "All tests executed.";
+        public const string DefaultTestSuite = "DEFAULT";
+        public const int DefaultTestPage = 130455;
+        public int TestPage { get; private set; }
+        public string TestSuite { get; private set; }
+        public TestRunner(string serviceUrl, AuthenticationScheme authenticationScheme, ICredentials credential,
+            TimeSpan interactionTimeout, string culture) : base(serviceUrl, authenticationScheme, credential, interactionTimeout, culture) 
+        { 
+        }
+
+        public void SetupTestRun(int testPage = DefaultTestPage, string testSuite = DefaultTestSuite, string extensionId = "", string testCodeunitsRange = "",
+            string testProcedureRange = "", int testRunnerCodeunit = 130450, DisabledTest[] disabledTests = null, bool stabilityRun = false)
+        {
+            TestPage = TestPage;
+            TestSuite = testSuite; ;
+
+            OpenTestForm(testPage);
+            SetTestSuite(OpenedForm, testSuite);
+            SetExtensionId(OpenedForm, extensionId);
+            SetTestCodeunits(OpenedForm, testCodeunitsRange);
+            SetTestProcedures(OpenedForm, testProcedureRange);
+            SetTestRunner(OpenedForm, testRunnerCodeunit);
+            SetRunFalseOnDisabledTests(OpenedForm, disabledTests);
+            SetStabilityRun(OpenedForm, stabilityRun);
+            ClearTestResults(OpenedForm);
+            
+            // TODO: Codecoverage settings if enabled!
+
+            //CloseForm(form);
+        }
+
+        public string RunNextTest()
+        {
+            OpenTestForm(TestPage);
+            SetTestSuite(OpenedForm, TestSuite);
+            InvokeAction(GetActionByName(OpenedForm, "RunNextTest"));
+            var testResultControl = GetControlByName(OpenedForm, "TestResultJson");
+            return testResultControl.StringValue;
+        }
+
+        public void RunAllTests()
+        {
+            bool runNextTest = true;
+
+            while ( runNextTest )
+            {
+                var testResult = RunNextTest();
+                Console.WriteLine(testResult);
+                Console.WriteLine();
+                runNextTest = (testResult != AllTestsExecutedString);
+            }
+        }
+
+        public override void CloseSession()
+        {
+            TestPage = 0;
+            TestSuite = "";
+
+            base.CloseSession();
+        }
+
+        protected ClientLogicalForm OpenTestForm(int testPage = 130455)
+        {
+            OpenForm(testPage);
+            if (OpenedForm == null)
+            {
+                throw new Exception($"Cannot open page {testPage}. Verify if the test tool and test objects are imported and can be opened manually.");
+            }
+            return OpenedForm;
+        }
+
+        protected void SetTestSuite(ClientLogicalForm form, string testSuite = "DEFAULT")
+        {
+            var suiteControl = GetControlByName(form, "CurrentSuiteName");
+            SaveValue(suiteControl, testSuite);
+        }
+
+        protected void SetExtensionId(ClientLogicalForm form, string extensionId)
+        {
+            if (string.IsNullOrEmpty(extensionId))
+            {
+                return;
+            }
+
+            var extensionIdControl = GetControlByName(form, "ExtensionId");
+            SaveValue(extensionIdControl, extensionId);
+        }
+
+        protected void SetTestCodeunits(ClientLogicalForm form, string testCodeunitsFilter)
+        {
+            if (string.IsNullOrEmpty(testCodeunitsFilter))
+            {
+                return;
+            }
+
+            var testCodeunitRangeFilterControl = GetControlByName(form, "TestCodeunitRangeFilter");
+            SaveValue(testCodeunitRangeFilterControl, testCodeunitsFilter);
+        }
+
+        protected void SetTestProcedures(ClientLogicalForm form, string filter)
+        {
+            var control = GetControlByName(form, "TestProcedureRangeFilter");
+            SaveValue(control, filter);
+        }
+
+        protected void SetTestRunner(ClientLogicalForm form, int testRunnerId)
+        {
+            if (testRunnerId == 0)
+            {
+                return;
+            }
+
+            var testRunnerCodeunitIdControl = GetControlByName(form, "TestRunnerCodeunitId");
+            SaveValue(testRunnerCodeunitIdControl, testRunnerId.ToString());
+        }
+
+        protected void SetRunFalseOnDisabledTests(ClientLogicalForm form, DisabledTest[] disabledTests)
+        {
+            if ((disabledTests == null) || (disabledTests.Length == 0))
+            {
+                return;
+            }
+
+            foreach ( var disabledTestMethod in disabledTests)
+            {
+                var testKey = disabledTestMethod.CodeunitName + "," + disabledTestMethod.Method;
+                var removeTestMethodControl = GetControlByName(form, "DisableTestMethod");
+                SaveValue(removeTestMethodControl, testKey);
+            }
+        }
+        protected void SetStabilityRun(ClientLogicalForm form, bool stabilityRun)
+        {
+            var stabilityRunControl = GetControlByName(form, "StabilityRun");
+            SaveValue(stabilityRunControl, stabilityRun.ToString());
+        }
+
+        protected void ClearTestResults(ClientLogicalForm form)
+        {
+            this.InvokeAction(GetActionByName(form, "ClearTestResults"));
+        }
+    }
+}
