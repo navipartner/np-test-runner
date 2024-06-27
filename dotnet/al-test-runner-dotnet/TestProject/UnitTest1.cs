3bc86bd5e5650b45affe1ee37d134d3eb831bb44
Jakub Vaňák <jakubv@navipartner.dk>
2024-06-27 14:51:37 +0400

    Unit tests

--- dotnet/al-test-runner-dotnet/TestProject/UnitTest1.cs
+++ dotnet/al-test-runner-dotnet/TestProject/UnitTest1.cs
@@ -1,4 +1,6 @@
+using NaviPartner.ALTestRunner;
 using NaviPartner.ALTestRunner.HttpZipStream;
+using System.Net;
 
 namespace TestProject
 {
@@ -12,6 +14,14 @@ namespace TestProject
                 @"c:/temp/HttpZipStreamTest/", 
                 @"(?i)Applications\\testframework\\TestRunner\\Internal\\.*\.dll$");
             Console.WriteLine("Done");
+        }
+
+        [Fact]
+        public void TestResolveAssemblies()
+        {
+            AssemblyResolver.SetupAssemblyResolve("Microsoft.Dynamics.Framework.UI.Client",
+                "C:/Users/JakubVanak/Documents/Repos/NaviPartner/np-al-test-runner-fork/.npaltestrunner/CSLibs/24.0.16410.18056");
+            var testRunner = new TestRunner("https://whatever", "UserNamePassword", new NetworkCredential(), TimeSpan.FromSeconds(60), "");
         }
     }
 }
